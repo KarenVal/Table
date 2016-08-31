9af1e275783af8e8788e79b9f7c9a24c70196886
Ximena <Ximena@macbook-pro-de-macintosh.local>
2016-08-30 23:58:27 -0500

    Initial Commit

---
+++ Intro Table/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Intro Table
+//
+//  Created by Ximena on 30/08/16.
+//  Copyright © 2016 KaVa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
